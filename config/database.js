0caffbf10a7c4d7256bc344be704c1cb27f98314
Byoungcheon Lee <lbcsultan@gmail.com>
2021-02-24 22:01:12 +0900

    1st

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // database: 'mongodb://localhost:27017/meanauth',
+  // database: 'mongodb://sultan:sultan123456@ds151066.mlab.com:51066/meanauth2019',
+  database: 'mongodb+srv://test:test1111@cluster0-unlns.mongodb.net/test?retryWrites=true&w=majority',
+  secret: 'yoursupersecret-dfljksdlfkj'
+}
